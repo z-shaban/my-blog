df48d213ec0850e79538b83c2e014b6ab1ccc2a4
shaban <146794146+sarzhh@users.noreply.github.com>
2025-12-23 10:44:33 +0000

    fetch some blog posts for the homepage

--- backend/routes/homepage.js
+++ backend/routes/homepage.js
@@ -1,9 +1,19 @@
+import { prisma } from '../lib/prisma.js';
 import { Router } from "express";
 
 const homepage = Router();
 
-homepage.get('/',(req,res)=>{
-    res.json('homepage')
+homepage.get('/',async(req,res)=>{
+    try{
+        const posts = await prisma.posts.findMany({
+            take: 3
+        })
+         res.json(posts)
+    }catch(error){
+        console.error(error)
+        res.status(500).json({error:'server error'})
+    }
+   
 })
 
 export {homepage}
