8052514fcc1f705c4e815108f3bd54a75d13c64e
shaban <146794146+sarzhh@users.noreply.github.com>
2025-12-28 15:56:30 +0000

    display all post on posts page

--- userfrontend/src/components/posts.jsx
+++ userfrontend/src/components/posts.jsx
@@ -1,9 +1,64 @@
+import { useEffect, useState} from "react"
+import { API_URL } from "../config"
+import {Link} from 'react-router'
+
+
+
 function Posts(){
+    const [posts,setPosts] = useState(null)
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        async function homePage() {
+            try{
+                const response = await fetch(`${API_URL}/blogs`)
+                const data = await response.json()
+                if(!response.ok){ 
+                    throw new Error(data.error || 'something went wrong')
+                }
+                setPosts(data)
+                
+            }catch(error){
+               setError(error)
+               console.error(error)
+            } finally{
+                setLoading(false)
+            }
+        }
+        homePage()
+    },[])
+
+   if(loading) return <p>Loading....</p>
+   if(error) return <p>{error.message} </p>
     return(
-        <>
-        <h1>Blog Posts</h1>
+        <> 
+        <div className="text-slate-100 flex flex-1 flex-col items-center justify-center ">
+             <h1 className="mt-8 mb-8">ALL POSTS</h1>
+         
+        {posts && (
+        <div>
+            
+                {posts.map((post)=>{
+                    return <div key={post.id} className="border border-slate-700 w-[80vw] p-4 mb-4 bg-slate-800">
+                       <p>{post.title} </p> 
+                       <p>{new Date(post.publishedAt).toLocaleDateString()} </p>
+                        </div>
+                })}
+            
+        </div>
+        )} 
+      
+        </div>
+       
         </>
+       
+       
     )
 }
+
+
+
+
 
 export {Posts}
